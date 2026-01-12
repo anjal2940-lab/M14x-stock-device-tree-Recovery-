54113006cff0996b94324ded15c1e25e2d7b0598
anjal2940-lab <anjal2940@gmail.com>
2026-01-13 00:07:21 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo pbrp_m14x-user
-add_lunch_combo pbrp_m14x-userdebug
-add_lunch_combo pbrp_m14x-eng
+# Vendorsetup for m14x - Choices are now handled by AndroidProducts.mk
