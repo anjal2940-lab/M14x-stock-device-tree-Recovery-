e6f5f6e2a11082e44e4bec0f6539b6c9b53428b5
anjal2940-lab <anjal2940@gmail.com>
2026-01-12 23:59:22 +0530

    Update pbrp_m14x.mk

--- pbrp_m14x.mk
+++ pbrp_m14x.mk
@@ -1,16 +1,9 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/utils/config/pbrp.mk)
+# Inherit PBRP common bits (The Fix for your error)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from m14x device
 $(call inherit-product, device/samsung/m14x/device.mk)
