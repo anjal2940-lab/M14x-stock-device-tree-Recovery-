87ac0bb5a76b5c367f25a03e56c9d5ef7e34e58c
anjal2940-lab <anjal2940-lab@users.noreply.github.com>
2026-01-12 17:54:27 +0000

    Standardize tree structure for PBRP/TWRP build 2aee53efcd4b5cac5930a9bf004b601f17337a14

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pbrp_m14x.mk
+
+COMMON_LUNCH_CHOICES := \
+    pbrp_m14x-user \
+    pbrp_m14x-userdebug \
+    pbrp_m14x-eng
